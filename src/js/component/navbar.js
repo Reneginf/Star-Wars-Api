c4de8095ec0cddb6482e02a897332cf3399c4d81
Reneginf <reneginf@gmail.com>
2022-09-22 18:40:46 +0000

    Final Draft

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  const { store, actions } = useContext(Context);
+
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <Link to="/">
+        <span className="navbar-brand mb-0 h1">STAR WARS</span>
+      </Link>
+      <div className="ml-auto">
+        <div className="btn-group">
+          <button
+            className="btn btn-primary btn-md dropdown-toggle"
+            type="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Favorites
+          </button>
+          <div className={store.favorites.length > 0? "dropdown-menu show" : "d-none"}> 
+            <ul>
+              {store.favorites.length > 0? store.favorites.map((favorite, index) => (
+                <li key={index}>
+                  <span>{favorite.name}</span>
+                  <span onClick={() => deleteFav(favorite.name)}>delete</span>
+                </li>
+              )):null}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -10,6 +10,7 @@ export const Demo = () => {
 
 	return (
 		<div className="container">
+			<h1> Browse Databank</h1>
 			<ul className="list-group">
 				{store.demo.map((item, index) => {
 					return (
@@ -18,7 +19,7 @@ export const Demo = () => {
 							className="list-group-item d-flex justify-content-between"
 							style={{ background: item.background }}>
 							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
+								<span>{item.title}</span>
 							</Link>
 							{// Conditional render example
 							// Check to see if the background is orange, if so, display the message
@@ -27,9 +28,6 @@ export const Demo = () => {
 									Check store/flux.js scroll to the actions to see the code
 								</p>
 							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
 						</li>
 					);
 				})}
